4323f9d5260cf94a5787f9c18857d9ac0e67db17
Morgan Navarro <msn2139@columbia.edu>
2018-03-19 20:35:44 +0000

    Test

--- server.py
+++ server.py
@@ -147,7 +147,7 @@ def index():
 
 @app.route('/full_list')
 def full_list():
-	cursor = g.conn.execute()
+	cursor = g.conn.execute("SELECT *")
 	cursor.execute("SELECT * FROM player")
 	rows = cursor.fetchall()
 	widths = []
