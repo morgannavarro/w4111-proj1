6888b920f3c76a129438b08977107c557239de76
Morgan Navarro <msn2139@columbia.edu>
2018-03-19 21:02:47 +0000

    Fixed output error

--- server.py
+++ server.py
@@ -152,7 +152,7 @@ def full_list():
 	results = []
 	results.append("{}   {}   {}   {}   {}   {}   {}   {}".format("player_id", "name", "position", "hometown", "dob", "height", "number", "team_id"))
 	for row in rows:
-		results.append(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+		results.append(row)
 	cursor.close()
 	context = dict(data = results)
 	return render_template("full_list.html", **context)
